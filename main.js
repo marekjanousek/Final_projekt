81f257f702bbec507b14a30aa1452cd82805e581
marekjanousek <marekjanousbravo@gmail.com>
2026-06-06 01:50:36 +0200

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-// jemný fade-in efekt
-document.addEventListener("DOMContentLoaded", () => {
-  document.body.style.opacity = "0";
-  setTimeout(() => {
-    document.body.style.transition = "0.6s";
-    document.body.style.opacity = "1";
-  }, 50);
-});
